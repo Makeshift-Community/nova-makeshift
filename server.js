e4589d81a0e9274df2755a07b5f56e62b4781bb1
NKN1396 <someone.took.nkn1396@gmail.com>
2022-01-08 21:55:34 +0100

    Remove NKN1396/discord.js-handles

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 //External dependencies
 const commando = require("discord.js-commando")
-const discordErrorHandler = require("discord.js-handles")
 
 //Load info
 const token = require("./token.json")	//I'm an idiot - Thanks for the lesson
-const makeshift = require("./src/resources/makeshift.json")
 
 //Create new Discord client
 const makeshiftbot = new commando.Client({
@@ -15,22 +13,6 @@ const makeshiftbot = new commando.Client({
 
 //Load and register commands.
 require("./src/commands/index")(makeshiftbot)
-
-let handlerOptions = {
-	logAllGuilds: false,
-	guilds: [
-		makeshift.guild
-	],
-	name: "nova-public",
-	notify: {
-		users: [
-			"153595272465743872"
-		],
-		channels: []
-	}
-}
-
-discordErrorHandler(makeshiftbot, handlerOptions)
 
 //Load custom modules
 require("./src/modules/index")(makeshiftbot)
