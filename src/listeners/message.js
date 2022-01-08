b8a179dbfcf0f6c98d657e72aad7a044dc8c727a
NKN1396 <someone.took.nkn1396@gmail.com>
2022-01-09 00:48:49 +0100

    Refactor sassynova.js

--- src/listeners/message.js
+++ src/listeners/message.js
@@ -1,9 +1,11 @@
 import handleSarcasm from '../modules/sarcasm.js'
+import handle8ball from '../modules/8ball.js'
 
 export default function (client) {
   client.on("message", handle)
 }
 
-const handle = function (message) {
-  handleSarcasm(message)
+const handle = async function (message) {
+  await handleSarcasm(message)
+  await handle8ball(message)
 }
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,5 @@
 module.exports = function (client) {
 	require("./greet")(client)
-	require("./sassynova")(client)
-	require("./sarcasm")(client)
 	require("./soontm")(client)
 	require("./voicemagic")(client)
 	require("./prism2")(client)
