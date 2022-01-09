2cb7570ecfef68fabfbd69fc874466fa031412a7
NKN1396 <someone.took.nkn1396@gmail.com>
2022-01-09 04:00:26 +0100

    Fix broken check for Makeshift guild

--- src/modules/greet.js
+++ src/modules/greet.js
@@ -1,8 +1,12 @@
+import { GUILD as GUILD_ID, TEXTCHANNELS } from '../resources/makeshift.js'
+const CHANNEL_ID = TEXTCHANNELS.GENERAL
+
 export default async function (member) {
   // Check if member joined Makeshift guild
-  if (member.guild.id !== makeshift.guild) return
+  const guild = member.guild
+  if (guild.id !== GUILD_ID) return
 
-  const channel = await member.guild.channels.fetch()
+  const channel = await guild.channels.fetch(CHANNEL_ID)
     .catch(console.error)
   if (channel === undefined) { return }
 
