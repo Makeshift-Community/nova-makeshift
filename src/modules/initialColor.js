4d9d5898ec55adb85c8f3cdb52ad37fd0c893287
NKN1396 <someone.took.nkn1396@gmail.com>
2022-01-09 01:08:48 +0100

    Refactor prism2.js

---
+++ src/modules/initialColor.js
@@ -0,0 +1,16 @@
+// Dependencies
+import { size } from "lodash"
+
+const makeshift = require("../resources/makeshift.json")
+const colors = makeshift.roles?.colors
+
+export default async function(member){
+
+	//Check if member joined Makeshift guild
+	if(member.guild.id !== makeshift.guild) return
+
+	const color = colors[BigInt(guildMember.user.id) % size(colors)]
+
+	guildMember.roles.add(color)
+		.catch(console.error)
+}
