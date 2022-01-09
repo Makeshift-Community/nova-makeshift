facd3c60f41ced316200f2fa3d19dad30fbb9a09
NKN1396 <someone.took.nkn1396@gmail.com>
2022-01-09 04:01:07 +0100

    Add check for message on TextChannel

--- src/modules/sarcasm.js
+++ src/modules/sarcasm.js
@@ -2,6 +2,7 @@ const trigger = /(?:^\/s$)|(?:\s\/s$)/g
 
 export default async function (message) {
   // Check
+  if (message.guild === undefined) { return }
   if (trigger.test(message.content) === false) { return }
 
   message.channel.send('*(That was sarcasm)*')
