8890e585e8383731cf4fe1ee38afe3f23fc8d592
NKN1396 <someone.took.nkn1396@gmail.com>
2022-01-09 04:27:35 +0100

    Fix old code preventing startup

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ import { Client, Intents } from 'discord.js'
 // Custom dependencies
 import token from './src/resources/token.js' // I'm an idiot, thanks for the lesson
 import registerModules from './src/listeners/index.js'
-import registerCommands from './src/commands/index.js'
 
 const makeshiftbot = new Client({
   intents: [Intents.FLAGS.GUILD_MESSAGES]
 })
 
 registerModules(makeshiftbot)
-registerCommands(makeshiftbot)
 
 // Start bot
 makeshiftbot.login(token)
