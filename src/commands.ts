a091771604035c28ef3e3c0bef2f68aefb6c3400
NKN1396 <someone.took.nkn1396@gmail.com>
2023-11-25 18:47:34 +0100

    feat: Add resources command

--- src/commands.ts
+++ src/commands.ts
@@ -2,5 +2,11 @@ import Command from "./Command.js";
 import testCommand from "./commands/test.js";
 import evaluateCommand from "./commands/evaluate.js";
 import searchWikiCommand from "./commands/searchWiki.js";
+import resourcesCommand from "./commands/resources.js";
 
-export default [testCommand, evaluateCommand, searchWikiCommand] as Command[];
+export default [
+  testCommand,
+  evaluateCommand,
+  searchWikiCommand,
+  resourcesCommand,
+] as Command[];
