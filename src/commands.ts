0510e02a68810e0b014f67a630e3f90c280ddd7b
NKN1396 <someone.took.nkn1396@gmail.com>
2023-11-26 16:02:08 +0100

    fix: Add rules command missing

--- src/commands.ts
+++ src/commands.ts
@@ -3,10 +3,12 @@ import testCommand from "./commands/test.js";
 import evaluateCommand from "./commands/evaluate.js";
 import searchWikiCommand from "./commands/searchWiki.js";
 import resourcesCommand from "./commands/resources.js";
+import rulesCommand from "./commands/rules.js";
 
 export default [
   testCommand,
   evaluateCommand,
   searchWikiCommand,
   resourcesCommand,
+  rulesCommand,
 ] as Command[];
