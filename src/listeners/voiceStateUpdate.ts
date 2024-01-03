13edb39cd40ca57f7b21b0138abc3752e44b1077
NKN1396 <someone.took.nkn1396@gmail.com>
2024-01-03 18:34:20 +0100

    feat: Add activity logging

--- src/listeners/voiceStateUpdate.ts
+++ src/listeners/voiceStateUpdate.ts
@@ -1,5 +1,6 @@
 import { Client, Events, VoiceState } from "discord.js";
 import handleVoice from "../modules/assignEmptyVoiceChannel.js";
+import { logVoiceActivity as handleActivityLogging } from "../modules/logActivity.js";
 
 export default function (client: Client) {
   client.on(Events.VoiceStateUpdate, handle);
@@ -7,4 +8,5 @@ export default function (client: Client) {
 
 const handle = async function (oldState: VoiceState, newState: VoiceState) {
   await handleVoice(oldState, newState).catch(console.error);
+  handleActivityLogging(oldState, newState).catch(console.error);
 };
