2c69ce831ebdb5823dd85d405313a235fb3dbdab
NKN1396 <someone.took.nkn1396@gmail.com>
2024-01-05 21:32:39 +0100

    chore: Build version 7.2.0

--- dist/listeners/messageCreate.js
+++ dist/listeners/messageCreate.js
@@ -1,7 +1,6 @@
 import handleSarcasm from "../modules/sarcasmDisclaimer.js";
 import handle8ball from "../modules/8ball.js";
 import handleSoon from "../modules/reactToMessagesEndingWithSoon.js";
-import { logMessageActivity as handleActivityLogging } from "../modules/logActivity.js";
 import { Events } from "discord.js";
 export default function (client) {
     client.on(Events.MessageCreate, handle);
@@ -10,5 +9,4 @@ const handle = async function (message) {
     await handleSarcasm(message).catch(console.error);
     await handle8ball(message).catch(console.error);
     await handleSoon(message).catch(console.error);
-    handleActivityLogging(message).catch(console.error);
 };
