e3322820fbb1ed770103abf6e7e4b70e9582b5de
NKN1396 <someone.took.nkn1396@gmail.com>
2026-05-31 23:15:29 +0200

    build!: Remove old compiled files

--- dist/listeners/voiceStateUpdate.js
+++
@@ -1,8 +0,0 @@
-import { Events } from "discord.js";
-import handleVoice from "../modules/maintainEmptyVoiceChannel.js";
-export default function (client) {
-    client.on(Events.VoiceStateUpdate, handle);
-}
-const handle = async function (oldState, newState) {
-    await handleVoice(oldState, newState).catch(console.error);
-};
